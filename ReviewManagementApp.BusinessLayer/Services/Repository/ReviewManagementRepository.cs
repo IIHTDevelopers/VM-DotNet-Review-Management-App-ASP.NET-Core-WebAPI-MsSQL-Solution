62ee13c420b90009b802ed9c26ea0e94ea6338eb
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-14 11:28:21 +0530

    Add files via upload

---
+++ ReviewManagementApp.BusinessLayer/Services/Repository/ReviewManagementRepository.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewManagementApp.BusinessLayer.ViewModels;
+using ReviewManagementApp.DataLayer;
+using ReviewManagementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ReviewManagementApp.BusinessLayer.Services.Repository
+{
+    public class ReviewManagementRepository : IReviewManagementRepository
+    {
+        private readonly ReviewManagementAppDbContext _dbContext;
+        public ReviewManagementRepository(ReviewManagementAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Review> CreateReview(Review ReviewModel)
+        {
+            try
+            {
+                var result = await _dbContext.Reviews.AddAsync(ReviewModel);
+                await _dbContext.SaveChangesAsync();
+                return ReviewModel;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<bool> DeleteReviewById(long id)
+        {
+            try
+            {
+                _dbContext.Remove(_dbContext.Reviews.Single(a => a.ReviewId== id));
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public List<Review> GetAllReviews()
+        {
+            try
+            {
+                var result = _dbContext.Reviews.
+                OrderByDescending(x => x.ReviewId).Take(10).ToList();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<Review> GetReviewById(long id)
+        {
+            try
+            {
+                return await _dbContext.Reviews.FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+       
+        public async Task<Review> UpdateReview(ReviewViewModel model)
+        {
+            var Review = await _dbContext.Reviews.FindAsync(model.ReviewId);
+            try
+            {
+
+                _dbContext.Reviews.Update(Review);
+                await _dbContext.SaveChangesAsync();
+                return Review;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
---
+++ ReviewManagementApp.BusinessLayer/Services/ReviewManagementService.cs
@@ -0,0 +1,46 @@
+﻿using ReviewManagementApp.BusinessLayer.Interfaces;
+using ReviewManagementApp.BusinessLayer.Services.Repository;
+using ReviewManagementApp.BusinessLayer.ViewModels;
+using ReviewManagementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewManagementApp.BusinessLayer.Services
+{
+    public class ReviewManagementService : IReviewManagementService
+    {
+        private readonly IReviewManagementRepository _repo;
+
+        public ReviewManagementService(IReviewManagementRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Review> CreateReview(Review employeeReview)
+        {
+            return await _repo.CreateReview(employeeReview);
+        }
+
+        public async Task<bool> DeleteReviewById(long id)
+        {
+            return await _repo.DeleteReviewById(id);
+        }
+
+        public List<Review> GetAllReviews()
+        {
+            return  _repo.GetAllReviews();
+        }
+
+        public async Task<Review> GetReviewById(long id)
+        {
+            return await _repo.GetReviewById(id);
+        }
+
+        public async Task<Review> UpdateReview(ReviewViewModel model)
+        {
+           return await _repo.UpdateReview(model);
+        }
+    }
+}
